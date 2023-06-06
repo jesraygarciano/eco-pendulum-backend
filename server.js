c41fc142e2598b6f4cef4bbfe14aa29ab0fa4585
Jesray Garciano <jesraygarciano@Jesrays-MacBook-Pro.local>
2023-06-06 16:58:45 +0800

    Eco pendulum back-end Integration

---
+++ server.js
@@ -0,0 +1,32 @@
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+});
+
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
+
+// const express = require('express');
+// const shopify = require('@shopify/shopify-api');
+
+// const app = express();
+
+// app.get('/', async (req, res) => {
+//   // Get the customer data from Shopify.
+//   const customer = await shopify.Customer.get(req.query.id);
+
+//   // Send the customer data back to the client.
+//   res.json(customer);
+// });
+
+// app.listen(3000, () => {
+//   console.log('Server listening on port 3000');
+// });
+
