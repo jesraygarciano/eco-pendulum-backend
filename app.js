a5be56d511b2c628e1499f507e8aa1868125d734
Jesray Garciano <jesraygarciano@Jesrays-MacBook-Pro.local>
2023-06-06 17:32:02 +0800

    update

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 
+const express = require('express');
+const shopify = require('@shopify/shopify-api');
 const http = require('http');
 
 const server = http.createServer((req, res) => {
@@ -7,24 +9,27 @@ const server = http.createServer((req, res) => {
     res.end('Hello World');
 });
 
+console.log("awawaw");
+
+const app = express();
+
+app.get('/', async (req, res) => {
+  // Get the customer data from Shopify.
+  const customer = await shopify.Customer.get(req.query.id);
+
+  // Send the customer data back to the client.
+  res.json(customer);
+});
+
 const port = process.env.PORT || 3000;
 
 server.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
 
-// const express = require('express');
-// const shopify = require('@shopify/shopify-api');
 
-// const app = express();
 
-// app.get('/', async (req, res) => {
-//   // Get the customer data from Shopify.
-//   const customer = await shopify.Customer.get(req.query.id);
 
-//   // Send the customer data back to the client.
-//   res.json(customer);
-// });
 
 // app.listen(3000, () => {
 //   console.log('Server listening on port 3000');
